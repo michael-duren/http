fd58d1c6a887bc6db040125393603cce7d166d32
Michael Duren <michaeld@michaelduren.com>
2026-02-14 12:31:18 -0600

    feat:update tests to use custom reader

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,6 +1,7 @@
 package request
 
 import (
+	"io"
 	"strings"
 	"testing"
 
@@ -10,7 +11,11 @@ import (
 
 func TestRequestLineParse(t *testing.T) {
 	t.Run("Good GET Request line", func(t *testing.T) {
-		r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+		reader := &chunkReader{
+			data:            "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+			numBytesPerRead: 3,
+		}
+		r, err := RequestFromReader(reader)
 		require.NoError(t, err)
 		require.NotNil(t, r)
 		assert.Equal(t, "GET", r.RequestLine.Method)
@@ -19,7 +24,11 @@ func TestRequestLineParse(t *testing.T) {
 	})
 
 	t.Run("Good GET Request line with path", func(t *testing.T) {
-		r, err := RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+		reader := &chunkReader{
+			data:            "GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+			numBytesPerRead: 1,
+		}
+		r, err := RequestFromReader(reader)
 		require.NoError(t, err)
 		require.NotNil(t, r)
 		assert.Equal(t, "GET", r.RequestLine.Method)
@@ -28,7 +37,12 @@ func TestRequestLineParse(t *testing.T) {
 	})
 
 	t.Run("Invalid number of parts", func(t *testing.T) {
-		_, err := RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+		_, err := RequestFromReader(
+			&chunkReader{
+				data:            "/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+				numBytesPerRead: 4,
+			},
+		)
 		require.Error(t, err)
 	})
 
@@ -51,4 +65,23 @@ func TestRequestLineParse(t *testing.T) {
 		_, err := RequestFromReader(strings.NewReader("get /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 		require.Error(t, err)
 	})
+}
+
+type chunkReader struct {
+	data            string
+	numBytesPerRead int
+	pos             int
+}
+
+func (cr *chunkReader) Read(p []byte) (n int, err error) {
+	if cr.pos >= len(cr.data) {
+		return 0, io.EOF
+	}
+
+	endIndex := min(cr.pos+cr.numBytesPerRead, len(cr.data))
+
+	n = copy(p, cr.data[cr.pos:endIndex])
+	cr.pos += n
+
+	return n, nil
 }
