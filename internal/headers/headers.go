b308faf34a07b1536a0f3ca67f9040ef155eb818
Michael Duren <michaeld@michaelduren.com>
2026-02-21 13:12:30 -0600

    feat: update header parsing & test

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -14,6 +14,10 @@ func NewHeaders() Headers {
 	return make(map[string]string)
 }
 
+// Parse parses a slice of bytes passed should be one line
+// of a header
+// Returns n the number of bytes read, if it completed parsing
+// and a possible error
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	rn := bytes.Index(data, []byte(parsing.CLRF))
 	if rn == -1 {
@@ -35,10 +39,29 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 		return 0, false, fmt.Errorf("invalid key: %s", key)
 	}
 
-	h[key] = value
+	h[strings.ToLower(key)] = value
 	return rn + 2, false, nil
 }
 
 func isValidKey(s string) bool {
-	return len(s) > 0 && s[0] != ' ' && s[len(s)-1] != ' '
+	if len(s) == 0 {
+		return false
+	}
+	for _, c := range s {
+		if !isTokenChar(c) {
+			return false
+		}
+	}
+	return true
+}
+
+func isTokenChar(c rune) bool {
+	switch c {
+	case '!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+		return true
+	}
+	if c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' || c >= '0' && c <= '9' {
+		return true
+	}
+	return false
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -14,9 +14,29 @@ func TestParse(t *testing.T) {
 		n, done, err := headers.Parse(data)
 		require.NoError(t, err)
 		require.NotNil(t, headers)
-		assert.Equal(t, "localhost:42069", headers["Host"])
+		assert.Equal(t, "localhost:42069", headers["host"])
 		assert.Equal(t, 23, n)
 		assert.False(t, done)
+	})
+
+	t.Run("Handles invalid chars", func(t *testing.T) {
+		headers := NewHeaders()
+		data := []byte("broken-💔: broken heart\r\n\r\n")
+		n, done, err := headers.Parse(data)
+		require.Error(t, err)
+		require.False(t, done)
+		require.Equal(t, 0, n)
+	})
+
+	t.Run("Map does not contain any upper case keys", func(t *testing.T) {
+		headers := NewHeaders()
+		testStr := "BROKEN: broken heart\r\n\r\n"
+		data := []byte(testStr)
+		n, done, err := headers.Parse(data)
+		require.NoError(t, err)
+		require.False(t, done)
+		require.Equal(t, len(data)-2, n)
+		require.Equal(t, "broken heart", headers["broken"])
 	})
 
 	t.Run("Invalid spacing header", func(t *testing.T) {
